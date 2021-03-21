6654d6e63c9cb0b39fba7893c3c775bb006aeafb
Reece Fleck <reecef1215@gmail.com>
2021-03-20 22:49:59 -0400

    Began Redesign of UserProfile
    Added new dependencies and layout. Not functional at this point.

--- Screens/UserProfile.js
+++ Screens/UserProfile.js
@@ -1,11 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, Alert } from 'react-native';
+import { View, Alert } from 'react-native';
+import { Text, Button, withStyles } from 'react-native-ui-kitten'
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const UserProfile = (props) => {
     const [profile, setProfile] = useState(null);
     const [fetched, setFetched] = useState(false);
+    const themedStyle = withStyles(theme => ({
+        root: {
+          backgroundColor: theme['color-basic-100'],
+          marginTop: 60
+        },
+        header: {
+          alignItems: 'center',
+          paddingTop: 25,
+          paddingBottom: 17
+        },
+        userInfo: {
+          flexDirection: 'row',
+          paddingVertical: 18
+        },
+        bordered: {
+          borderBottomWidth: 1,
+          borderColor: theme['color-basic-400']
+        },
+        section: {
+          flex: 1,
+          alignItems: 'center'
+        },
+        space: {
+          marginBottom: 3,
+          color: theme['color-basic-1000']
+        },
+        separator: {
+          backgroundColor: theme['color-basic-400'],
+          alignSelf: 'center',
+          flexDirection: 'row',
+          flex: 0,
+          width: 1,
+          height: 42
+        },
+        buttons: {
+          flexDirection: 'row',
+          paddingVertical: 8
+        },
+        button: {
+          flex: 1,
+          alignSelf: 'center'
+        },
+        text: {
+          color: theme['color-basic-1000']
+        }
+      }))
+
 
     const createDeleteAlert = () =>
         Alert.alert(
@@ -90,21 +138,71 @@ const UserProfile = (props) => {
     }
 
     return (
-        <View>
-            {
-                profile &&
-                <>
-                    <Text>Name: {profile.name}</Text>
-                    <Text>Username: {profile.username}</Text>
-                    <Text>Points: {profile.eventPoints}</Text>
-                    <Button title='Modify Account' onPress={() => props.navigation.navigate('Settings')} />
-                    <Button title='Create an organization' onPress={() => props.navigation.navigate('CreateOrg')} />
-                    <Button title='Log Out' onPress={logOut} />
-                    <Button title='Delete Account' onPress={createDeleteAlert} />
-                </>
-            }
+        <View style={themedStyle.root}>
+          <View style={[themedStyle.header, themedStyle.bordered]}>
+            <Text category='h6' style={themedStyle.text}>
+              Test User
+            </Text>
+          </View>
+          <View style={[themedStyle.userInfo, themedStyle.bordered]}>
+            <View style={themedStyle.section}>
+              <Text appearance='hint' category='s2'>
+                Points
+              </Text>
+            </View>
+            <View style={themedStyle.section}>
+              <Text category='s1' style={themedStyle.space}>
+                0
+              </Text>
+              <Text appearance='hint' category='s2'>
+                Friends
+              </Text>
+            </View>
+            <View style={themedStyle.section}>
+              <Text category='s1' style={themedStyle.space}>
+                0
+              </Text>
+              <Text appearance='hint' category='s2'>
+                Following
+              </Text>
+            </View>
+          </View>
+          <View style={themedStyle.buttons}>
+          <Button
+              style={themedStyle.button}
+              appearance='ghost'
+              status='danger'
+              onPress={() => props.navigation.navigate('Settings')}>
+              MODIFY ACCOUNT
+            </Button>
+            <Button
+              style={themedStyle.button}
+              appearance='ghost'
+              status='danger'
+              onPress={() => props.navigation.navigate('CreateOrg')}>
+              CREATE ORGANIZATION
+            </Button>
+            <Button
+              style={themedStyle.button}
+              appearance='ghost'
+              status='danger'
+              onPress={logOut}>
+              LOGOUT
+            </Button>
+            <Button
+              style={themedStyle.button}
+              appearance='ghost'
+              status='danger'
+              onPress={createDeleteAlert}>
+              DELETE
+            </Button>
+            <View style={themedStyle.separator} />
+            <Button style={themedStyle.button} appearance='ghost' status='danger'>
+              MESSAGE
+            </Button>
+          </View>
         </View>
-    )
+      )
 }
 
 export default UserProfile;
