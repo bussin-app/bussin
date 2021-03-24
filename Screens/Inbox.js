1b7a747730a9b9ef3d0fa0df798d410525eb01c5
Reece Fleck <32523443+refleck15@users.noreply.github.com>
2021-03-24 10:46:56 -0400

    Added Inbox to Navigation

---
+++ Screens/Inbox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+const Inbox = (props) => {
+  return (
+    <SafeAreaView>
+        <Text>See Friend Requests and Invites Here!</Text>
+    </SafeAreaView>
+  )
+  }
+
+export default Inbox;
