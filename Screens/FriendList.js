d472c0fb9ce279c36086a9fcdb38654840414038
Reece Fleck <32523443+refleck15@users.noreply.github.com>
2021-03-24 11:17:00 -0400

    Added FriendList.js navigation

---
+++ Screens/FriendList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+const FriendList = (props) => {
+  return (
+    <SafeAreaView>
+        <Text>See Your List of Friends Here!</Text>
+    </SafeAreaView>
+  )
+  }
+
+export default FriendList;
