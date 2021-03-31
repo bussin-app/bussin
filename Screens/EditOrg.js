2aab32dac3474e371ef672e5605ccc92b2ec2acf
Sukriti Rai <rai27@purdue.edu>
2021-03-30 19:51:41 -0500

    Finished Edit organization UI

---
+++ Screens/EditOrg.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, LogBox, Alert } from 'react-native';
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
+import FormContainer from "../Shared/Form/FormContainer";
+import Input from "../Shared/Form/Input";
+
+const EditOrg = (props) => {
+    const [name, setName] = useState("");
+
+    const createDeleteAlert = () =>
+        Alert.alert(
+            "Delete Event",
+            "You will not be able to recover this event.",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                { text: "Delete", onPress: deleteEvent }
+            ],
+            { cancelable: false }
+        );
+
+    const deleteEvent = async () => {
+        // Deletion linking
+    };
+
+    const updateEvent = async () => {
+        // Construct data for backend
+    };
+
+    return (
+        <KeyboardAwareScrollView
+            viewIsInsideTabBar={true}
+            extraHeight={200}
+            enableOnAndroid={true}
+        >
+            <FormContainer title={"Edit Event"}>
+                <Input
+                    placeholder={"Enter Name of Event"}
+                    name={"name"}
+                    id={"name"}
+                    onChangeText={(text) => setName(text)}
+                    value={name}
+                />
+                <View style={{ marginTop: 50 }}>
+                    <Button title={"Save"} onPress={updateEvent} />
+                    <Button title='Delete Organization' onPress={createDeleteAlert} />
+                </View>
+            </FormContainer>
+        </KeyboardAwareScrollView>
+    )
+}
+
+export default EditOrg;
