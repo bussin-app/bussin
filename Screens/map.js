40866cd8eedf6defc0c4715de730dd316c397d44
qiren zhao <zhaoqirenjisoo@gmail.com>
2021-03-30 21:33:40 -0400

    map proto

---
+++ Screens/map.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps'
+
+const map = (props) => {
+    return (
+        <MapView style={{ ...StyleSheet.absoluteFillObject}}>
+
+        </MapView>
+    )
+}
+
+export default map;
