dcc23e69791d59ef34575416e6bc19f5e85eaad8
Reece Fleck <32523443+refleck15@users.noreply.github.com>
2021-04-02 10:35:35 -0400

    Styling fixes

--- Screens/Inbox.js
+++ Screens/Inbox.js
@@ -130,11 +130,11 @@ const Inbox = (props) => {
           shadowRadius: 20
         }}>
           {filter == 'friends' && <Text style={{ fontWeight: "200", fontSize: 25, fontFamily: 'HelveticaNeue' }} onPress={() => getItem(item)}>
-            Friend Request From</Text>}
+            Friend Request From:</Text>}
           {filter == 'invites' && item.type == 'organizations' && <Text style={{ fontWeight: "200", fontSize: 25, fontFamily: 'HelveticaNeue' }} onPress={() => getItem(item)}>
-            Organization Invite From</Text>}
+            Organization Invite From:</Text>}
           {filter == 'invites' && item.type == 'event' && <Text style={{ fontWeight: "200", fontSize: 25, fontFamily: 'HelveticaNeue' }} onPress={() => getItem(item)}>
-            Event Invite From</Text>}
+            Event Invite From:</Text>}
           <Text style={{ fontSize: 20, fontFamily: 'HelveticaNeue' }} onPress={() => getItem(item)}>
             {item.from.name}  ({item.from.username})
         </Text>
