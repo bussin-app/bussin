dd77692370539a8f054b9c6eb5bfae485a8dc516
Blake Johnson <blake.k.johnson.4@gmail.com>
2021-04-01 22:55:45 -0400

    changed id for invite response to _id

--- Screens/Inbox.js
+++ Screens/Inbox.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, StyleSheet, View, FlatList, StatusBar, Button} from 'react-native';
+import { SafeAreaView, Text, StyleSheet, View, FlatList, StatusBar, Button } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Inbox = (props) => {
@@ -7,9 +7,9 @@ const Inbox = (props) => {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(['friends']);
   const [title, setTitle] = useState(['friends']);
-  
 
- 
+
+
 
   const fetchRequests = async () => {
     let storedToken = await AsyncStorage.getItem('@bussin-token');
@@ -17,10 +17,10 @@ const Inbox = (props) => {
     setToken(storedToken);
 
     let response = await fetch("https://bussin.blakekjohnson.dev/api/friends/friendInbox", {
-            method: "GET",
-            headers: {
-              'Authorization': `Bearer ${storedToken}`,
-            },
+      method: "GET",
+      headers: {
+        'Authorization': `Bearer ${storedToken}`,
+      },
     });
 
     // Convert response to JSON
@@ -37,10 +37,10 @@ const Inbox = (props) => {
 
     console.log('Before the fetch');
     let response = await fetch("https://bussin.blakekjohnson.dev/api/invites/inviteInbox", {
-            method: "GET",
-            headers: {
-              'Authorization': `Bearer ${storedToken}`,
-            },
+      method: "GET",
+      headers: {
+        'Authorization': `Bearer ${storedToken}`,
+      },
     });
 
     // Convert response to JSON
@@ -51,53 +51,53 @@ const Inbox = (props) => {
     setData(response);
   };
 
-  
+
 
   const replyRequest = async (status, item) => {
     if (filter == 'friends') {
       let res = await fetch('https://bussin.blakekjohnson.dev/api/friends/friendRespond', {
-     method: 'DELETE',
-     body: JSON.stringify({
-       request: {
-          to: item.to, 
-          from: item.from._id,
-          response: status
-       } 
-     }),
-     headers: {
-       'Authorization': `Bearer ${token}`,
-       'Content-Type': 'application/json'
-     }
-    });
-     res = await res.json();
-     console.log(res);
-
-    } else {
-      let res = await fetch('https://bussin.blakekjohnson.dev/api/invites/respond', {
         method: 'DELETE',
         body: JSON.stringify({
-        invite: {
-            to: item.to, 
+          request: {
+            to: item.to,
             from: item.from._id,
-            type: item.type,
-            response: status,
-            id: item._id
-        } 
+            response: status
+          }
         }),
         headers: {
           'Authorization': `Bearer ${token}`,
           'Content-Type': 'application/json'
         }
       });
-     res = await res.json();
-     console.log(res);
+      res = await res.json();
+      console.log(res);
+
+    } else {
+      let res = await fetch('https://bussin.blakekjohnson.dev/api/invites/respond', {
+        method: 'DELETE',
+        body: JSON.stringify({
+          invite: {
+            to: item.to,
+            from: item.from._id,
+            type: item.type,
+            response: status,
+            _id: item._id
+          }
+        }),
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      });
+      res = await res.json();
+      console.log(res);
     }
-    
+
   };
 
   useEffect(() => {
     props.navigation.addListener('focus', () => {
-    fetchRequests();
+      fetchRequests();
     });
   }, []);
 
@@ -116,30 +116,30 @@ const Inbox = (props) => {
   const ItemView = ({ item }) => {
     return (
       <SafeAreaView>
-      <View style={{
-        width: 350, padding: SPACING, marginBottom: SPACING, backgroundColor: 'rgba(255, 255, 255, 0.7)', borderRadius: 12,
-        shadowColor:"#355070",
-        shadowOffset: {
-          width: 0,
-          height: 10
-        },
-        shadowOpacity: .3,
-        shadowRadius: 20
-      }}>
-        <Text style={{ fontWeight: "200", fontSize: 25, fontFamily: 'HelveticaNeue' }} onPress={() => getItem(item)}>
-        { (filter == 'invites')? 'Event Invite From:' : 'Friend Request From:' }
+        <View style={{
+          width: 350, padding: SPACING, marginBottom: SPACING, backgroundColor: 'rgba(255, 255, 255, 0.7)', borderRadius: 12,
+          shadowColor: "#355070",
+          shadowOffset: {
+            width: 0,
+            height: 10
+          },
+          shadowOpacity: .3,
+          shadowRadius: 20
+        }}>
+          <Text style={{ fontWeight: "200", fontSize: 25, fontFamily: 'HelveticaNeue' }} onPress={() => getItem(item)}>
+            {(filter == 'invites') ? 'Event Invite From:' : 'Friend Request From:'}
+          </Text>
+          <Text style={{ fontSize: 20, fontFamily: 'HelveticaNeue' }} onPress={() => getItem(item)}>
+            {item.from.name}  ({item.from.username})
         </Text>
-        <Text style={{ fontSize: 20, fontFamily: 'HelveticaNeue' }} onPress={() => getItem(item)}>
-          {item.from.name}  ({item.from.username})
-        </Text>
-        { filter == 'invites' && <Text style={{ fontWeight: "200", fontSize: 25, fontFamily: 'HelveticaNeue' }}>To:</Text>}
-        { filter == 'invites' && <Text style={{  fontSize: 20, fontFamily: 'HelveticaNeue' }}>{item.foreignID.name}</Text>}
-        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-          <Button title={"Accept"} onPress={() => replyRequest(1, item)}></Button>
-          <Button title={"Deny"} onPress={() => replyRequest(2, item)}></Button>
+          {filter == 'invites' && <Text style={{ fontWeight: "200", fontSize: 25, fontFamily: 'HelveticaNeue' }}>To:</Text>}
+          {filter == 'invites' && <Text style={{ fontSize: 20, fontFamily: 'HelveticaNeue' }}>{item.foreignID.name}</Text>}
+          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+            <Button title={"Accept"} onPress={() => replyRequest(1, item)}></Button>
+            <Button title={"Deny"} onPress={() => replyRequest(2, item)}></Button>
+          </View>
+
         </View>
-        
-      </View>
       </SafeAreaView>
     );
   };
@@ -158,7 +158,7 @@ const Inbox = (props) => {
   };
 
   const getItem = (item) => {
-      props.navigation.navigate('viewUserProfile', { user: item });
+    props.navigation.navigate('viewUserProfile', { user: item });
   };
 
   if (!token) {
@@ -168,7 +168,7 @@ const Inbox = (props) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ alignItems: 'center' }}>
-        <Button title={ (filter != 'friends')? 'Invites' : 'Friend Request' }  onPress = {() => changeFilter(filter)} />
+        <Button title={(filter != 'friends') ? 'Invites' : 'Friend Request'} onPress={() => changeFilter(filter)} />
       </View>
       <View style={{ alignItems: 'center' }}>
         <FlatList
