8b7f886f4c987fe46fcf580a6ebf80e5465e204d
Sukriti Rai <rai27@purdue.edu>
2021-04-21 18:52:42 -0500

    Removed Sort In MembersList

--- Screens/ViewEvent.js
+++ Screens/ViewEvent.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, Button, StyleSheet, SafeAreaView, Linking} from "react-native";
-
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
